7a770b884659efa644aed43fe3b536bcc08efcbf
eddymadu14 <edwardmaduneme.com>
2026-01-07 16:28:59 +0100

    changes to baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,15 @@
 
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+if (!API_BASE_URL) {
+  throw new Error("VITE_API_BASE_URL is not defined in .env");
+}
+
+
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${API_BASE_URL}`,
 });
 
 export const fetchLeads = () => api.get("/leads");
